15861e50059f97ab16c621d0702ea2ab6ea0cd32
Sanil Shrestha <sanilshrestha@lftechnology.com>
2014-01-09 22:18:16 +0545

    controller, action, view and routing managed for album module

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -3,13 +3,13 @@
 namespace Album;
 
 class Module {
-
-    public function getAutoloaderConfig() {
     
+    public function getAutoloaderConfig() {
+
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
+            /* 'Zend\Loader\ClassMapAutoloader' => array(
+              __DIR__ . '/autoload_classmap.php',
+              ), */
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -6,6 +6,22 @@ return array(
             'Album\Controller\Album' => 'Album\Controller\AlbumController',
         ),
     ),
+    'routers' => array(// routes for module starts here
+        'routes' => array(
+            'album' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/album[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',),
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action' => 'index',),
+                ),
+            ),
+        ),
+    ), // routes for module ends here
     'view_manager' => array(
         'template_path_stack' => array(
             'album' => __DIR__ . '/../view',
